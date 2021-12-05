06d5c8b6c8b22dab1815f24ee641c75c92c63e16
IvanKorchmit <criperadmiral92@gmail.com>
2021-12-05 23:16:44 +0200

    Добавьте файлы проекта.

---
+++ SharpRPG/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SharpRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public abstract class Tile
+    {
+        private string name;
+        private string description;
+
+
+        public string Name => name;
+        public string Description => description;
+
+        public Tile(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+    }
+    
+}
