574c7b68b541f9d28d293a6c18ecddfc902147b7
IvanKorchmit <criperadmiral92@gmail.com>
2021-12-06 00:32:48 +0200

    Moaarr !!!!!!!!!!!1 classes :DDDD

--- SharpRPG/Program.cs
+++ SharpRPG/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
-
+using System.IO;
 namespace SharpRPG
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game.Init();
+            
+        }
+    }
+
+    public static class Game
+    {
+        private static readonly string FILE_PATH;
+        public const string FILE_NAME = "world.txt";
+        private const int WIDTH = 16;
+        private const int HEIGHT = 16;
+        private static Tile[,] world;
+        public static Player player;
+        static Game()
+        {
+            FILE_PATH = @$"{Path.GetDirectoryName(Environment.CurrentDirectory)}\{FILE_NAME}";
+            string contents = File.ReadAllText(FILE_PATH);
+            Console.WriteLine(contents);
+            world = WorldParser.Parse(contents, WIDTH, HEIGHT);
+        }
+
+        public static void Init()
+        {
+
+        }
+
+    }
+    public static class WorldParser
+    {
+        public static Tile[,] Parse(string input, int width, int height)
+        {
+            Tile[,] result = new Tile[width, height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    int index = width * x + y;
+                    char c = input[index];
+                    switch (c)
+                    {
+                        case ' ':
+                            result[x, y] = default;
+                            break;
+                        case '@':
+                            result[x, y] = new Enemy(new Coord(x, y), "Skeleton", "Spooky", 10, 3);
+                            break;
+                        case 'P':
+                            Game.player = new Player(new Coord(x, y));
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+            }
+
+
+            return result;
         }
     }
 }
--- SharpRPG/Tile.cs
+++ SharpRPG/Tile.cs
@@ -1,4 +1,7 @@
-﻿namespace SharpRPG
+﻿using System;
+
+
+namespace SharpRPG
 {
     public abstract class Tile
     {
@@ -15,5 +18,67 @@
             this.description = description;
             this.position = position;
         }
+    }
+    public abstract class Entity : Tile, IDamagable
+    {
+        private int health;
+        private int damage;
+        public Entity(Coord position, string name, string description, int health, int damage) : base(name, description, position)
+        {
+            this.health = health;
+            this.damage = damage;
+
+        }
+
+        public int Health => health;
+        public void Attack(IDamagable target)
+        {
+            Random rand = new Random();
+            int damage = rand.Next(0, this.damage);
+            target.Damage(damage);
+        }
+        public void Damage(int damage)
+        {
+            health -= damage;
+            CheckHealth();
+        }
+        private void CheckHealth()
+        {
+            if (health <= 0)
+            {
+
+            }
+        }
+    }
+
+    public class Player : Entity
+    {
+        private BaseItem[] inventory;
+
+
+        public Player(Coord position) : base(position, "Player", "You.", 10, 1)
+        {
+            inventory = new BaseItem[10];
+        }
+
+    }
+    public class Enemy : Entity
+    {
+        public Enemy(Coord position, string name, string description, int health, int damage) : base (position,name,
+            description,health,damage)
+        {
+
+        }
+    }
+
+    public abstract class BaseItem : Tile
+    {
+        private int price;
+        public BaseItem(string name, string description, Coord position, int price) : base (name, description, position)
+        {
+            this.price = price;
+
+        }
+        public abstract void Use(Entity user);
     }
 }
